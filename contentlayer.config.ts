879c91aae0ff96814237f3309fe1f16f2c315e52
jaytech989 <jay.vyas@techchefz.com>
2025-04-28 17:59:07 +0530

    updated config files for deploymenmt

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -143,6 +143,7 @@ export const Authors = defineDocumentType(() => ({
     linkedin: { type: 'string' },
     github: { type: 'string' },
     layout: { type: 'string' },
+    medium: { type: 'string' },
   },
   computedFields,
 }))
